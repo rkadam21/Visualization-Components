660a60cc3b8c269a187d05472c99cc1b17e8f006
Rahul Kadam <coderk21@gmail.com>
2017-04-05 15:13:31 +0100

    Modifications for bar_chart.js
    Addition of variables for margin, colors, background-color,

--- js/bar_chart.js
+++ js/bar_chart.js
@@ -1,12 +1,18 @@
 function barChart(){
-    var width = 400,
-        height = 600
-
+    var width = 720,
+        height = 400
+    var margin = {
+      top: 30,
+      right: 10,
+      bottom: 30,
+      left: 50
+    }
     function chart(selection) {
         var data = selection.enter().data();
         var div = selection,
             svg = div.selectAll('svg');
-        svg.attr('width', width).attr('height', height);
+        svg .attr('width', width).attr('height', height)
+            .style('background', '#bce8f1')
 
         var tooltip = selection
             .append("div")
@@ -20,16 +26,24 @@ function barChart(){
             .style("font-family", "monospace")
             .style("width", "400px")
             .text("");
+        var barwidth = width - margin.left - margin.right
+        var barheight = height - margin.top - margin.bottom
+        var colors = d3.scaleLinear()
+            .domain([0, data.length * .33, data.length * .66, data.length])
+            .range(['#d6e9c6', '#bcb5ff', '#faebcc', '#ebccd1'])
 
         var xScale = d3.scaleBand()
             .domain(d3.range(data.length))
-            .range([0,width]);
+            .range([0, barwidth])
 
         var yScale = d3.scaleLinear()
             .domain([0, d3.max(data)])
-            .range([0, height])
+            .range([0, barheight])
 
-        var bar = svg.selectAll("rect")
+        var bar = svg.append('g')
+            .attr('transform',
+                'translate(' + margin.left + ', ' + margin.top + ')')
+            .selectAll("rect")
             .data(data)
             .enter()
             .append("rect")
@@ -37,11 +51,13 @@ function barChart(){
                 return xScale(i);
             })
             .attr("y", function(d){
-                return height - yScale(d);
+                return barheight - yScale(d);
             })
             .attr("width", xScale.bandwidth())
             .attr("height", function(d){ return yScale(d)})
-            .attr("fill",function(d){return "rgb(0,0,"+ d*10 +")"})
+            .style("fill", function(d, i){ return colors(i);})
+            .style("stroke", "#31708f")
+            .style("stroke-width", "1")
     }
 
     chart.width = function(value) {
