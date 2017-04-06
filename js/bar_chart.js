965d5f1b06b083edebff9493fce621994764798e
Rahul Kadam <coderk21@gmail.com>
2017-04-06 17:09:23 +0100

    Update to bar_chart.js for passing xAxis and yAxis columns
    Update to bar_chart.js for passing xAxis and yAxis columns

--- js/bar_chart.js
+++ js/bar_chart.js
@@ -1,46 +1,44 @@
 function barChart(){
     var width = 720,
-        height = 400
-    var margin = {
+        height = 400,
+    margin = {
       top: 30,
       right: 10,
       bottom: 30,
       left: 50
-    }
+    },
+    columnX = "name",
+    columnY = "value";
     function chart(selection) {
         var data = selection.enter().data();
         var div = selection,
-            svg = div.selectAll('svg');
-        svg .attr('width', width).attr('height', height)
-            .style('background', '#99c7a0')
+            svg = div.selectAll("svg");
+        svg .attr("width", width).attr("height", height)
+            .style("background", "#99c7a0")
 
         var tooltip = selection
             .append("div")
-            .style("position", "absolute")
-            .style("visibility", "hidden")
-            .style("color", "white")
-            .style("padding", "8px")
-            .style("background-color","#626D71")
-            .style("border-radius", "6px")
-            .style("text-align", "left")
-            .style("font-family", "monospace")
+            .attr("class", "tooltip")
             .style("width", "150px")
             .text("");
 
-        var barwidth = width - margin.left - margin.right
-        var barheight = height - margin.top - margin.bottom
+        var chartWidth = width - margin.left - margin.right
+        var chartHeight = height - margin.top - margin.bottom
+        var barWidth = chartWidth / data.length
 
         var colors = d3.scaleLinear()
             .domain([0, data.length * .33, data.length * .66, data.length])
-            .range(['#96ceb4','#ffeead','#ff6f69','#ffcc5c'])
+            .range(["#96ceb4","#ffeead","#ff6f69","#ffcc5c"])
 
         var xScale = d3.scaleBand()
             .domain(d3.range(data.length))
-            .range([0, barwidth])
+            .range([0, chartWidth])
 
         var yScale = d3.scaleLinear()
-            .domain([0, d3.max(data)])
-            .range([0, barheight])
+            .domain([0, d3.max(data, function(d){
+                return +d[columnY];
+            })])
+            .range([0, chartHeight])
 
         var bar = svg.append('g')
             .attr('transform',
@@ -53,16 +51,16 @@ function barChart(){
                 return xScale(i);
             })
             .attr("y", function(d){
-                return barheight;
+                return chartHeight;
             })
-            .attr("width", xScale.bandwidth()-3)
+            .attr("width", xScale.bandwidth()-10)
             .attr("height", 0)
-            .style("fill", function(d, i){ return colors(i);})
+            .style("fill", 'rgb(200,75,75)')
             .style("stroke", "#31708f")
             .style("stroke-width", "1")
             .on("mouseover", function(d){
                 tooltip.html("This is tooltip text for data."+"<br>"
-                + "The datapoint is: " + d);
+                + "The datapoint is: " + d[columnY]);
                 return tooltip.style("visibility", "visible");
             })
             .on("mousemove", function() {
@@ -77,10 +75,10 @@ function barChart(){
 
         bar.transition()
         .attr("height", function(d){
-            return yScale(d);
+            return yScale(d[columnY]);
         })
         .attr("y", function(d){
-            return barheight - yScale(d);
+            return chartHeight - yScale(d[columnY]);
         })
         .delay(function(d, i){
             return i * 20;
@@ -88,7 +86,11 @@ function barChart(){
         .duration(2000)
         .ease(d3.easeElastic)
 
-        var xAxis = d3.axisBottom(xScale)
+        var xScaleAxis = d3.scaleBand()
+              .domain(data.map(function(el){return el.name;}))
+              .range([0, chartWidth])
+
+        var xAxis = d3.axisBottom(xScaleAxis)
             .ticks(data.size)
         d3.select("svg").append("g")
             .attr("id","xAxisG")
@@ -96,9 +98,12 @@ function barChart(){
             .attr("transform",
             "translate("+ margin.left + ", " + (height-margin.bottom) + ")")
 
+
         var yAxisScale = d3.scaleLinear()
-            .domain([0, d3.max(data)])
-            .range([barheight, 0])
+            .domain([0, d3.max(data, function(d){
+                return +d[columnY];
+            })])
+            .range([chartHeight, 0])
         var yAxis = d3.axisLeft(yAxisScale)
             .ticks(10)
         d3.select("svg").append("g")
