7ff073aedc332d3509edf569c7f70629b4334621
Rahul Kadam <coderk21@gmail.com>
2017-04-09 01:01:13 +0100

    Data mangling changes
    Data mangling changes

--- js/grp_bar_chart.js
+++ js/grp_bar_chart.js
@@ -7,6 +7,10 @@ function grpBarChart(){
         left: 50,
         right: 10
     }
+    var columnX="",
+    columnY="",
+    columnGrp="";
+
     function chart(selection){
         var data = selection.enter().data();
         var div = selection,
@@ -22,32 +26,94 @@ function grpBarChart(){
                 .attr("width", 100);
 
         var xAxis = d3.scaleBand()
-            .domain(data.map(function(el){ return el[columnX];}))
+            .domain(data.map(function(el, i){ return el.key;}))
             .rangeRound([0, chartWidth])
             .paddingInner(0.1);
 
+        var grpAxis = d3.scaleBand()
+            .domain(["Under 5 Years", "5 to 13 Years", "14 to 17 Years", "18 to 24 Years", "25 to 44 Years", "45 to 64 Years","65 Years and Over"])
+            .rangeRound([0, xAxis.bandwidth()])
+
         var yAxis = d3.scaleLinear()
-            .domain(d3.extent(data, function(d){return d[columnY];}))
-            .range([0, chartHeight])
+            .domain([0,10604510])
+            .rangeRound([chartHeight, 0]);
+
+        var colorAxis = d3.scaleOrdinal()
+            .domain(["Under 5 Years", "5 to 13 Years", "14 to 17 Years", "18 to 24 Years", "25 to 44 Years", "45 to 64 Years","65 Years and Over"])
+            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+        var overallG = svg .append("g")
+            .attr("id", "overallG")
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+
+        var grpG = overallG .selectAll("g")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("id",function(d){return "grp"+d.key;})
+            .attr("transform", function(d){
+                return "translate("+ xAxis(d.key) + ", 0)";
+            });
+    var bar = grpG  .selectAll("rect")
+            .data(function(d){return d.values;})
+            .enter()
+            .append("rect")
+            .attr("id", function(d){return "bar"+ d[columnGrp];})
+            .attr("x", function(d,i){
+                return grpAxis(d[columnGrp]);
+            })
+            .attr("y", function(d){
+                return yAxis(d[columnY]);
+            })
+            .attr("width", grpAxis.bandwidth())
+            .attr("height", function(d,i){
+                return chartHeight - yAxis(d[columnY]);
+            })
+            .attr("fill", function(d,i) {
+                return colorAxis(d[columnGrp]);
+            });
+
     }
 
-    chart.width = function(value){
-
+    chart.width = function(value) {
+        if(!arguments.length){
+            return width;
+        }
+        width = value;
+        return chart;
     }
 
-    chart.height = function(value){
+    chart.height = function(value) {
+        if(!arguments.length){
+            return height;
+            }
+        height = value;
+        return chart;
+        }
 
+    chart.columnX = function(value) {
+        if(!value){
+            return alert('X-axis variable needs to be provided');
+        }
+        columnX = value;
+        return chart;
     }
 
-    chart.columnX = function(value){
-
+    chart.columnY = function(value) {
+        if(!value){
+            return alert('Y-axis variable needs to be provided');
+        }
+        columnY = value;
+        return chart;
     }
 
-    chart.columnY = function(value){
-
+    chart.columnGrp = function(value) {
+        if(!value){
+            return alert('Group variable needs to be provided');
+        }
+        columnGrp = value;
+        return chart;
     }
-
-    chart.group = function(value){
-
-    }
+    return chart;
 }
