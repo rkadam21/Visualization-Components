c70967045168a2184c1221682128bbde3f552eb2
Rahul Kadam <coderk21@gmail.com>
2017-04-10 15:46:27 +0100

    Update to Grouped Bar Chart components
    Update to  Grouped Bar Chart components for addition of function
    domainList() to calculate values as arrays for domains for scales from a
    nested key/value data.

--- js/grp_bar_chart.js
+++ js/grp_bar_chart.js
@@ -24,57 +24,73 @@ function grpBarChart(){
         var tooltip = svg.append("div")
                 .attr("class", "tooltip")
                 .attr("width", 100);
+        var grp = domainList(data, columnGrp)
 
-        var xAxis = d3.scaleBand()
+        var xScale = d3.scaleBand()
             .domain(data.map(function(el, i){ return el.key;}))
             .rangeRound([0, chartWidth])
             .paddingInner(0.1);
 
-        var grpAxis = d3.scaleBand()
-            .domain(data.map(function(el) {return el.values.map(function(ele,i){return ele[columnGrp];})})[0])
-            .rangeRound([0, xAxis.bandwidth()])
+        var grpScale = d3.scaleBand()
+            .domain(grp)
+            .rangeRound([0, xScale.bandwidth()])
             .padding(0.05)
 
-        var yAxis = d3.scaleLinear()
-            .domain([0,10604510]).nice()
-            .rangeRound([chartHeight, 0]);
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(domainList(data, columnY))])
+            .range([chartHeight, 0]);
 
-        var colorAxis = d3.scaleOrdinal()
-            .domain(data.map(function(el) {return el.values.map(function(ele,i){return ele[columnGrp];})})[0])
+        var colScale = d3.scaleOrdinal()
+            .domain(grp)
             .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
         var overallG = svg .append("g")
             .attr("id", "overallG")
             .attr("width", chartWidth)
             .attr("height", chartHeight)
-            .attr("transform", "translate("+margin.left+", "+margin.top+")")
+            .attr("transform",
+                "translate("+margin.left+", "+margin.top+")")
 
         var grpG = overallG .selectAll("g")
             .data(data)
             .enter()
             .append("g")
-            .attr("id",function(d){return "grp"+d.key;})
+            .attr("id",function(d){return "grp_"+d.key;})
             .attr("transform", function(d){
-                return "translate("+ xAxis(d.key) + ", 0)";
+                return "translate("+ xScale(d.key) + ", 0)";
             });
-    var bar = grpG  .selectAll("rect")
+
+        var bar = grpG  .selectAll("rect")
             .data(function(d){return d.values;})
             .enter()
             .append("rect")
             .attr("id", function(d){return "bar"+ d[columnGrp];})
             .attr("x", function(d,i){
-                return grpAxis(d[columnGrp]);
-            })
+                return grpScale(d[columnGrp]);
+                })
             .attr("y", function(d){
-                return yAxis(d[columnY]);
-            })
-            .attr("width", grpAxis.bandwidth())
+                return yScale(d[columnY]);
+                })
+            .attr("width", grpScale.bandwidth())
             .attr("height", function(d,i){
-                return chartHeight - yAxis(d[columnY]);
-            })
+                return chartHeight - yScale(d[columnY]);
+                })
             .attr("fill", function(d,i) {
-                return colorAxis(d[columnGrp]);
-            });
+                return colScale(d[columnGrp]);
+                });
+
+        function domainList(d, column) {
+            var map = d.map(v =>
+                        v.values.map(c =>
+                            c[column]
+                            )
+                        )
+                    .reduce((a, b) => a.concat(b));
+            var arr = [...new Set(map)];
+            return arr;
+        }
+
+
 
     }
 
