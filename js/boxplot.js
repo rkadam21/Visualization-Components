efdc77402108fd01bab207ac9ac8e9f6199bb05e
Rahul Kadam <coderk21@gmail.com>
2017-04-15 16:47:15 +0100

    Updated boxplot.js with rect component
    Updated boxplot.js with rect component

--- js/boxplot.js
+++ js/boxplot.js
@@ -8,9 +8,9 @@ function boxplot(){
         bottom: 30,
         left: 30,
         right: 10
-    }
+        }
 
-    function chart(selection){
+    function chart(selection) {
         var data = selection.enter().data();
         var div = selection,
         svg = div.selectAll("svg")
@@ -26,26 +26,88 @@ function boxplot(){
             .attr("width", "100px")
             .text("");
 
-        var xAxis = d3.scaleBand()
-            .domain([data.map(function(d){return d[columnX];})])
+        var xScale = d3.scaleBand()
+            .domain(data.map(function(d){return d[columnX];}))
             .range([0, chartWidth]);
 
-        var yAxis = d3.scaleLinear()
-            .domain([0, d3.max(data, function(d){return d[columnY];})])
-            .range([chartHeight, 0])
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(data,
+                function(d){return d[columnY];})]).nice()
+            .range([chartHeight, 0]);
+
+        var overallG = svg.append("g")
+            .attr("id","overallG")
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("transform",
+            "translate("+ margin.left + ", "+ margin.top + ")");
+
+        var circles = overallG.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("name", function(d){return "cir_"+d.day;})
+            .attr("r", 2)
+            .attr("cx", function(d, i){return xScale(d.day);})
+            .attr("cy", function(d){ return yScale(d.median);})
+
+        var gBox = overallG.selectAll("g#gBox")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("id","gBox")
+            .attr("name", function(d){
+                return"g_" + d.day;
+            })
+            .attr("transform", function(d){
+                return "translate(" + xScale(d.day) + ", "
+                + yScale(d.median) + ")";})
+            .each(function(d, i){
+                d3.select(this)
+                .append("rect")
+                .attr("width", 20)
+                .attr("height", function(d){
+                    return yScale(d.q1) - yScale(d.q3);
+                })
+                .style("fill","none")
+                .style("stroke","black")
+
+            })
 
     }
 
-    chart.width = function() {
+    chart.width = function(value) {
+        if(!arguments.length){
+            return width;
+        }
+        width = value
+        return chart
+    }
+
+    chart.height = function(value) {
+        if(!arguments.length){
+            return height;
+        }
+        height = value
+        return chart
+    }
+
+    chart.columnX = function(value) {
+        if(!value){
+            return alert('X-axis variable needs to be provided');
+            }
+        columnX = value;
+        return chart;
+    }
+
+    chart.columnY = function(value) {
+        if(!value){
+            return alert('Y-axis variable needs to be provided');
+            }
+        columnY = value;
+        return chart;
 
     }
-    chart.height = function() {
+    return chart;
 
-    }
-    chart.columnX = function() {
-
-    }
-    chart.columnY = function(){
-
-    }
 }
