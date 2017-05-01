431344fa8aec12e8b19eda503ac754a86dcb9322
Rahul Kadam <coderk21@gmail.com>
2017-05-01 17:28:17 +0100

    Update to pie_radial_chart.js to add the radial component
    Update to pie_radial_chart.js to add the radial component.

--- js/pie_radial_chart.js
+++ js/pie_radial_chart.js
@@ -13,7 +13,7 @@ function piradial(){
         svg = div.selectAll("svg")
             .attr("width", width)
             .attr("height", height)
-            .style("background","rgb(225,225,225)")
+            //.style("background","rgb(225,225,225)")
 
         var tooltip = selection
             .append("div")
@@ -24,10 +24,11 @@ function piradial(){
         chartHeight = height - margin.top - margin.bottom,
         radius = Math.min(chartWidth, chartHeight) / 2;
 
-        var colScale = d3.scaleOrdinal(d3.schemeCategory20);
+        var colScale = d3.scaleOrdinal(d3.schemeCategory10);
+        var colScale1 = d3.scaleOrdinal(d3.schemeCategory20b);
 
         var arc = d3.arc()
-            .outerRadius(radius-10)
+            .outerRadius(radius-120)
             .innerRadius(0);
 
         var labelArc = d3.arc()
@@ -52,6 +53,31 @@ function piradial(){
         pieG.append("path")
             .attr("d", arc)
             .style("fill", function(d){return colScale(d.data);});
+
+
+        var arcOut = d3.arc()
+            .outerRadius(radius - 90)
+            .innerRadius(radius - 115)
+
+        var pieOut = d3.pie()
+            .sort(null)
+            .value(function(d) {return d;})
+
+        var overAllGOut = svg.append("g")
+            .attr("name", "overAllGOut")
+            .attr("transform", "translate("+
+                chartWidth / 2 + ", "+ chartHeight / 2 + ")");
+
+        var pieGOut = overAllGOut.selectAll(".arc")
+            .data(pie(data))
+            .enter()
+            .append("g")
+            .attr("class","arc")
+
+        pieGOut.append("path")
+            .attr("d", arcOut)
+            .style("fill", function(d){return colScale1(d.data);});
+
 
 
     }
